3b928bb689f97332c0b3d4ef051aad22634bd7de
Colin Cunningham <colin.199643@gmail.com>
2020-06-02 14:35:23 -0400

    Another Marvel Guy section completed

--- client/src/components/AMG/index.js
+++ client/src/components/AMG/index.js
@@ -1,22 +1,24 @@
 import React from "react";
 import "./style.css";
 import { TwitterTimelineEmbed, TwitterFollowButton } from "react-twitter-embed";
+import ReactPlayer from "react-player"
+import podimg from "./Podacts.jpeg"
 
 function amg() {
   return (
     <>
       <div id="img"> </div>
       <div className="btn-group">
-        <TwitterFollowButton screenName={"AnotherMarvelG"} />{" "}
+        <TwitterFollowButton  screenName={"AnotherMarvelG"} />{" "}
         <a href="https://www.youtube.com/anothermarvelguy" rel="noopener noreferrer" target="_blank">
-          <button class="ui youtube button">
-            <i class="youtube icon"></i>
+          <button className="ui youtube button">
+            <i className="youtube icon"></i>
             Youtube
           </button>
         </a>
         <a href="https://www.instagram.com/anothermarvelg/" rel="noopener noreferrer" target="_blank">
-          <button class="ui instagram button">
-            <i class="instagram icon"></i>
+          <button className="ui instagram button">
+            <i className="instagram icon"></i>
             Instagram
           </button>
         </a>
@@ -27,7 +29,7 @@ function amg() {
             <p>Welcome to my website! My name is Alex Semp and I am AnotherMarvelGuy. In a world full of fans and creators making content about the movies and shows they love, I decided to become Another one (get it?). </p>
           </div>
           <div className="ui vertical segment">
-            <p>Along with my friend Tom G (The Other Marvel Guy), the creator of this website and the co-host of my <a href="" rel="noopener noreferrer" target="_blank" >podcast </a>, we strive to create a Marvel community built on positivity and shared interests. If this sounds good to you stay tuned, cause we have a whole bunch of video content coming. Thank you and I’ll catch y’all in a blip! </p>
+            <p>Along with my friend Tom G (The Other Marvel Guy), the creator of this website and the co-host of my <a href="https://linktr.ee/anothermarvelguy" rel="noopener noreferrer" target="_blank" >podcast </a>, we strive to create a Marvel community built on positivity and shared interests. If this sounds good to you stay tuned, cause we have a whole bunch of video content coming. Thank you and I’ll catch y’all in a blip! </p>
           </div>
         </div>
         <div id="twitter" className="col s12 m12 l4">
@@ -38,8 +40,25 @@ function amg() {
           />
         </div>
       </div>
-      <div class="col s12 m6 offset-m3 l4 offset-l4 z-depth-6">
-        <h1>Hi</h1><br/>
+      <div id="video" className="col s12 m12 l12">
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=ITuAcFY8mFk"
+        width="inherit"
+      />
+    </div>
+    <div className="row">
+    <div id="podcast" className="col s12 m6 l6">
+      <img src={podimg} id="podimg"alt="let"/>
+    </div>
+    <div id="podcast" className="col s12 m6 l6">
+    <div className="ui vertical segment">
+        <p style={{color: "black"}}>Talking about Marvel, the MCU, and some other off-topic conversations. Host: Semp, otherwise known as AnotherMarvelGuy on YouTube. Co-host: Colin, self-proclaimed as TOMG. New episodes every other week, Fridays at 10:00am. Catch y'all in a blip! </p>
+        <a style={{textDecoration: "underline"}}href="https://linktr.ee/anothermarvelguy" rel="noopener noreferrer" target="_blank" >Find us on your platform!</a>
+    </div>
+    <div className="ui vertical segment">
+    <iframe src="https://anchor.fm/anothermarvelguy/embed"  title="podcast" height="102px" width="inherit" frameBorder="0" scrolling="no"></iframe> 
+     </div>
+    </div>
     </div>
     </>
   );
